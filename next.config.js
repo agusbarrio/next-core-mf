71c2c52eb35e248525f1199be46587bdc864d639
Agustin Barrio <agustin.b@solunika.com>
2023-09-06 01:21:45 -0300

    microfrontend config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
+const NextFederationPlugin = require('@module-federation/nextjs-mf')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, options) => {
+
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: 'next_core_mf',
+        filename: "static/chunks/remoteEntry.js",
+        remotes: {},
+        exposes: {
+          './Button': './src/components/Button/index.tsx',
+        },
+        shared: {},
+      })
+    )
+    return config
+  }
 }
 
 module.exports = nextConfig
